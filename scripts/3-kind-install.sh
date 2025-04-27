0debb887a6733a68f3151fe9af0f28ba020fa949
hellocloudio <helloocloud@gmail.com>
2025-04-27 13:40:13 +0800

    Update 3-kind-install.sh

--- scripts/3-kind-install.sh
+++ scripts/3-kind-install.sh
@@ -2,9 +2,24 @@
 
 echo "[Running kind-install.sh]"
 
-# sudo curl -L "https://kind.sigs.k8s.io/dl/v0.11.1/kind-$(uname)-amd64" -o /usr/local/bin/kind
-# sudo curl -L "https://kind.sigs.k8s.io/dl/v0.14.0/kind-$(uname)-amd64" -o /usr/local/bin/kind
-# sudo curl -L "https://kind.sigs.k8s.io/dl/v0.15.0/kind-$(uname)-amd64" -o /usr/local/bin/kind
-sudo curl -L "https://kind.sigs.k8s.io/dl/v0.27.0/kind-$(uname)-amd64" -o /usr/local/bin/kind
+KIND_VERSION="v0.27.0"  # Update this to the version you want to install
+OS=$(uname)
+ARCH=$(uname -m)
+
+# Map architecture names
+if [ "$ARCH" = "x86_64" ]; then
+    ARCH="amd64"
+elif [ "$ARCH" = "aarch64" ]; then
+    ARCH="arm64"
+fi
+
+echo "Detected OS: $OS, Architecture: $ARCH"
+echo "Installing kind $KIND_VERSION for $OS-$ARCH"
+
+# Download and install kind
+sudo curl -L "https://kind.sigs.k8s.io/dl/$KIND_VERSION/kind-$OS-$ARCH" -o /usr/local/bin/kind
 sudo chmod +x /usr/local/bin/kind
+
+# Verify installation
+echo "Installation complete. Verifying kind version:"
 kind version
